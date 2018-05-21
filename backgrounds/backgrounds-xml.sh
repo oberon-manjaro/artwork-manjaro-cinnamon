9af0e11da9b2ba84d820dbfb4d52e054db47556a
Ste74 <stefano@manjaro.org>
2018-05-21 22:11:22 +0200

    Changed theme name
    Signed-off-by: Ste74 <stefano@manjaro.org>

--- backgrounds/backgrounds-xml.sh
+++ backgrounds/backgrounds-xml.sh
@@ -2,35 +2,35 @@
 
 #set variable
 
-WALL=cinnamon
-ABSPATH=/usr/share/backgrounds/manjaro-$WALL/
+THEME=cinnamon
+ABSPATH=/usr/share/backgrounds/$THEME/
 
-if [ -e manjaro-$WALL.xml ]; then
-    rm manjaro-$WALL.xml
+if [ -e $THEME.xml ]; then
+    rm $THEME.xml
 fi
 
 # creating the head of backgrounds.xml
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <!DOCTYPE wallpapers SYSTEM \"gnome-wp-list.dtd\">
-<wallpapers>" > manjaro-$WALL.xml
+<wallpapers>" > $THEME.xml
 
 # looking for all pictures in DIRECTORY
-echo "OK. Now we are creating manjaro-$WALL.xml"
+echo "OK. Now we are creating $THEME.xml"
 
 # This script is looking for .png and .jpg files only, but you can add here another file types. The "<options>stretched</options>" should work best for unknow sized files.
 for i in $(ls *jpg); do #Note if we add png files simply add $(ls *png)
-echo "<wallpaper>
-<name>${ABSPATH}$i</name>
+echo "<wallpaper deleted="\"false\"">
+<name>${i::(-4)}</name>
 <filename>${ABSPATH}$i</filename>
   <options>stretched</options>
     <pcolor>#ffffff</pcolor>
     <scolor>#000000</scolor>
     <shade_type>solid</shade_type>
-</wallpaper>" >> manjaro-$WALL.xml
+</wallpaper>" >> $THEME.xml
 done
 
 # creating the bottom of backgrounds.xml
-echo "</wallpapers>" >> manjaro-$WALL.xml
+echo "</wallpapers>" >> $THEME.xml
 
 echo
 echo "#################################"
